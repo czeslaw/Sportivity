8fcf88d1689fe87388e255ea412255f5cf170ae4
czeslaw <piotr.nietrzebka@gmail.com>
2015-12-02 13:13:48 +0100

    Added logout functionality.

--- Sportify/Sportify/Classes/ViewControllers/Root/TTRootViewController.m
+++ Sportify/Sportify/Classes/ViewControllers/Root/TTRootViewController.m
@@ -11,19 +11,59 @@
 
 static NSString *const kIdentifierSegueRootToAuthenticate = @"kIdentifierSegueRootToAuthenticate";
 
+@interface TTRootViewController ()
+
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *barButtonItemLogout;
+
+@end
+
 @implementation TTRootViewController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	
+//TODO: hardcoded strings
+	self.barButtonItemLogout.title = @"Logout";
+}
 
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
 	
 	if ([PFUser currentUser].isAuthenticated) {
 		
-//		TODO: perform data fetch
+//TODO: perform data fetch
 	}
 	else {
 		
 		[self performSegueWithIdentifier:kIdentifierSegueRootToAuthenticate sender:self];
 	}
+}
+
+#pragma mark - Action handling
+
+- (IBAction)tapButtonLogout:(id)sender {
+
+	__weak typeof (self) weakSelf = self;
+	
+//TODO: add preogress handler
+	[PFUser logOutInBackgroundWithBlock:^(NSError *error){
+	
+		if (error) {
+//TODO: hardcoded strings
+			[[[UIAlertView alloc] initWithTitle:@"Something went wrong"
+										message:error.localizedDescription
+									   delegate:nil
+							  cancelButtonTitle:@"OK"
+							  otherButtonTitles:nil]
+			 show];
+		}
+		else {
+		
+			[weakSelf performSegueWithIdentifier:kIdentifierSegueRootToAuthenticate sender:weakSelf];
+		}
+	}];
 }
 
 @end
